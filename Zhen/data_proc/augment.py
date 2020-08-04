bfa5a98e3c2938596dff06753fb1d19f1e4caea2
Zhen Yu <yishon555@outlook.com>
2020-08-04 15:28:42 +1000

    Update augment.py

--- Zhen/data_proc/augment.py
+++ Zhen/data_proc/augment.py
@@ -5,6 +5,9 @@ import PIL
 import cv2
 import os
 import skimage
+import skimage.io as io
+import colorcorrect.algorithm as cca
+from colorcorrect.util import from_pil, to_pil
 import matplotlib.pyplot as plt
 from imgaug import augmenters as iaa
 import torchvision.transforms as transforms
@@ -13,23 +16,42 @@ from data_proc.inception_crop import InceptionCrop
 import albumentations as A
 # https://github.com/fabioperez/skin-data-augmentation/blob/master/train.py
 
-aug_parameters = OrderedDict({'affine': {'rotation': 90,
-                                         'shear': 20,
-                                         'scale': [0.8, 1.2]},
-                              'hflip': True,
-                              'vflip': True,
-                              'rotate': True,
-                              'color_trans': {'brightness': (0.7, 1.3),
-                                              'contrast': (0.7, 1.3),
-                                              'saturation': (0.7, 1.3),
-                                              'hue': (-0.1, 0.1)},
-                              'normalization': {'mean': (0.485, 0.456, 0.406),
-                                                'std': (0.229, 0.224, 0.225)},
-                              'size': 320,
-                              'scale': (0.8, 1.2),
-                              'ratio': (0.8, 1.2)
-                              }
-                             )
+
+class Microscope:
+    def __init__(self, p=(0.15, 0.45)):
+        self.p = p
+
+    def add_black_microscope(self, image):
+        image = np.array(image)
+        circle = cv2.circle((np.ones(image.shape) * 255).astype(np.uint8), (image.shape[0] // 2, image.shape[1] // 2),
+                            random.randint(image.shape[0] // 2 + 0, image.shape[0] // 2 + 50), (0, 0, 0), -1)
+        mask = circle - 255
+        mask = skimage.filters.gaussian(mask * 255, sigma=random.randint(5, 20))
+        image = np.multiply(image, mask)
+        image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX)
+        image = PIL.Image.fromarray(image.astype(np.uint8))
+        return image
+
+    def add_white_microscope(self, image):
+        image = cca.grey_world(from_pil(image))  # array np.uint8
+        circle = cv2.circle((np.ones(image.shape) * 255).astype(np.uint8), (image.shape[0] // 2, image.shape[1] // 2),
+                            random.randint(image.shape[0] // 2 - 10, image.shape[0] // 2 + 10), (0, 0, 0), -1)
+        mask = circle - 255
+        image[mask == 0] = 255
+        image = skimage.filters.gaussian(image, sigma=random.random())
+        image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX)
+        image = PIL.Image.fromarray(image.astype(np.uint8))
+        return image
+
+    def __call__(self, image):
+        ratio = random.random()
+        if ratio < self.p[0]:
+            image = self.add_white_microscope(image)
+
+        if self.p[0] < ratio < self.p[1]:
+            image = self.add_black_microscope(image)
+
+        return image
 
 
 class Random_rotate:
@@ -45,7 +67,7 @@ class Random_rotate:
 
 
 class Realistic_hair_aug:
-    def __init__(self, hair_library=None, p=0.4):
+    def __init__(self, hair_library=None, p=0.5):
         assert os.path.exists(hair_library)
         self.hair_array = np.load(hair_library)
         self.ratio = p
@@ -59,7 +81,8 @@ class Realistic_hair_aug:
         image_hair = cv2.bitwise_and(image_resize, image_resize, mask=mask)
         image_hair = skimage.filters.gaussian(image_hair, sigma=1)
         image_hair = cv2.resize(image_hair, image.shape[:2])
-        image_hair = image_hair*255
+        # image_hair = image_hair*255
+        image_hair = cv2.normalize(image_hair, None, 0, 255, cv2.NORM_MINMAX)
 
         return image_hair.astype(np.uint8)
 
@@ -70,7 +93,6 @@ class Realistic_hair_aug:
                 image = PIL.Image.fromarray(image)
             except TypeError:
                 image = image
-
         return image
 
 
@@ -132,9 +154,13 @@ class Augmentations:
         if augs['hair_mask'] is not None:
             transform_list.append(Realistic_hair_aug(hair_library=augs['hair_mask']))
 
+        if augs['microscope'] is not None:
+            transform_list.append(Microscope(p=augs['microscope']))
+
         # normalize to tensor
         transform_list.append(transforms.ToTensor())
         transform_list.append(transforms.Normalize(mean=self.mean, std=self.std))
+        self.debug_trans = transforms.Compose(transform_list[:-2])
         self.transform = transforms.Compose(transform_list)  # output: T, H, W, C
         self.ta_transform = self._get_crop_transform(tta)
 
@@ -164,3 +190,41 @@ class Augmentations:
         ])
 
 
+if __name__ == '__main__':
+    aug_parameters = OrderedDict({'affine': None,
+                                  'hflip': True,
+                                  'vflip': True,
+                                  'rotate': True,
+                                  'hair_mask': '/home/zyi/MedicalAI/ISIC_2020/hairs2/mask_array.npy',
+                                  'microscope': (0.1, 0.4),
+                                  'color_trans': {'brightness': (0.7, 1.3),
+                                                  'contrast': (0.7, 1.3),
+                                                  'saturation': (0.7, 1.3),
+                                                  'hue': (-0.1, 0.1)},
+                                  'normalization': {'mean': (0.485, 0.456, 0.406),
+                                                    'std': (0.229, 0.224, 0.225)},
+                                  'size': 320,
+                                  'scale': (0.7, 1.3),
+                                  'ratio': (0.7, 1.3)
+                                  }
+                                 )
+
+    augmentor = Augmentations(augs=aug_parameters)
+
+    # img = io.imread('/home/zyi/MedicalAI/ISIC_2020/data/test/ISIC_0165615.jpg')
+    # img = cv2.resize(img, (img.shape[1]//6, img.shape[0]//6))
+    # # img = cv2.resize(img, (320, 320))
+    # circle = cv2.circle((np.ones([320, 320, 3]) * 255).astype(np.uint8), (320 // 2, 320 // 2),
+    #                     random.randint(320 // 2 + 15, 320 // 2 + 30), (0, 0, 0), -1)
+    #
+    # im1 = cca.grey_world(img)
+    # print(im1.dtype)
+    # # mask = circle - 255
+    # circle = cv2.resize(circle, (img.shape[0], img.shape[1])).transpose(1,0,2)
+    # im1[circle == 255] = 0
+    # image = PIL.Image.fromarray(im1)
+    # image.show()
+    image = PIL.Image.open('/home/zyi/MedicalAI/ISIC_2020/data/test/ISIC_1336046.jpg')
+    img = augmentor.debug_trans(image)
+    plt.imshow(np.array(img))
+    plt.show()
